d69144ea918c15dafc6aed87881130d7931a0984
maixd7 <122345406+maixd7@users.noreply.github.com>
2023-05-26 00:26:25 -0700

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,5 @@
-from tensorflow.keras.preprocessing.image import img_to_array
-from tensorflow.keras.models import load_model
+from keras.preprocessing.image import img_to_array
+from keras.models import load_model
 import numpy as np
 import cv2
 import os
@@ -12,6 +12,8 @@ model = load_model('SmileDetection.model')
 webcam = cv2.VideoCapture(0)
     
 classes = ['anger', 'contempt', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+
+img_counter = 0
 
 # loop through frames
 while webcam.isOpened():
@@ -31,7 +33,7 @@ while webcam.isOpened():
         (endX, endY) = f[2], f[3]
 
         # draw rectangle over face
-        cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
+        cv2.rectangle(frame, (startX,startY), (endX,endY), (255,0,0), 2)
 
         # crop the detected face region
         face_crop = np.copy(frame[startY:endY,startX:endX])
@@ -39,13 +41,13 @@ while webcam.isOpened():
         if (face_crop.shape[0]) < 10 or (face_crop.shape[1]) < 10:
             continue
 
-        # preprocessing for gender detection model
+        # preprocessing for model
         face_crop = cv2.resize(face_crop, (96,96))
         face_crop = face_crop.astype("float") / 255.0
         face_crop = img_to_array(face_crop)
         face_crop = np.expand_dims(face_crop, axis=0)
 
-        # apply gender detection on face
+        # apply model
         conf = model.predict(face_crop)[0] # model.predict return a 2D matrix, ex: [[9.9993384e-01 7.4850512e-05]]
 
         # get label with max accuracy
@@ -53,7 +55,6 @@ while webcam.isOpened():
         label = classes[idx]
 
         label = "{}: {:.2f}%".format(label, conf[idx] * 100)
-
         Y = startY - 10 if startY - 10 > 10 else startY + 10
 
         # write label and confidence above face rectangle
@@ -61,10 +62,15 @@ while webcam.isOpened():
                     0.7, (0, 255, 0), 2)
 
     # display output
-    cv2.imshow("gender detection", frame)
+    cv2.imshow("smile detection", frame)
 
-    # press "Q" to stop
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    # press esc to stop and space to capture
+    if label == "happy":
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+    if cv2.waitKey(1)%256 == 27: 
         break
 
 # release resources
