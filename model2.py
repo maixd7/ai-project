74acf48aa1c9314ad9a4aaced1b7bc1d5c6bcc75
adam-hin <109179328+adam-hin@users.noreply.github.com>
2023-05-31 15:12:19 -0700

    Update model2.py

--- model2.py
+++ model2.py
@@ -34,7 +34,7 @@ for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 
-# image loading and resizing done by Adam
+# image loading, resizing, and adding labels done by Adam
 # initial parameters
 data = []
 labels = []
